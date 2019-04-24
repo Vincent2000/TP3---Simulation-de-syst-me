5a0302ac19779746d694d5d00e4f902915d88629
Unknown <objecris@gmail.com>
2019-04-23 22:31:49 -0400

    Fini

--- Parametres.h
+++ Parametres.h
@@ -3,11 +3,11 @@
 
 //#define VERBEUX //A décommenter si vous voulez le détail de chaque jour
 
-#define N 3
+#define N 1
 #define F 7
 #define NOMBRE_JOURS 100 //Nombres de jours de simulation
 #define HEURES_PAR_JOUR 10 //Nombres d'heures où le système est actif dans une journée
-#define LAMBDA_ARRIVEE 0.8 //taux moyen d'arrivée global en personnes par minute
+#define LAMBDA_ARRIVEE 0.5 //taux moyen d'arrivée global en personnes par minute
 #define MOYENNE_TEMPS_TRAVAIL 3600 //En secondes
 #define TEMPS_ENTRE_DEUX_ETAGES 10 //Nombre de secondes que met un ascenceur à se déplacer entre deux étages successifs
 
